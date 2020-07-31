35af500459486e0b5f38638374601db7f69a3595
Mostafa Khudair <59371810+mostafakhudair@users.noreply.github.com>
2020-07-31 16:06:10 +0200

    Update LocalAuthenticator.php

--- src/Authentication/LocalAuthenticator.php
+++ src/Authentication/LocalAuthenticator.php
@@ -1,64 +1,67 @@
-<?php namespace Myth\Auth\Authentication;
+<?php
+
+namespace Myth\Auth\Authentication;
 
 use CodeIgniter\Router\Exceptions\RedirectException;
-use \Config\Services;
-use Myth\Auth\Entities\User;
+use Config\Services;
 use Myth\Auth\Exceptions\AuthException;
 
-class LocalAuthenticator extends AuthenticationBase implements AuthenticatorInterface
+class LocalAuthenticator extends BaseAuthentication implements AuthenticatorInterface
 {
     /**
      * Attempts to validate the credentials and log a user in.
      *
-     * @param array $credentials
-     * @param bool  $remember Should we remember the user (if enabled)
+     * @param array   $credential
+     * @param boolean $remember   Should we remember the user (if enabled)
      *
-     * @return bool
+     * @return boolean
      */
-    public function attempt(array $credentials, bool $remember = null): bool
+    public function attempt(array $credential, bool $remember = false): bool
     {
-        $this->user = $this->validate($credentials, true);
-
+        if (! $this->validate($credential))
+        {
+            return false;
+        }
+        
         if (empty($this->user))
         {
-            // Always record a login attempt, whether success or not.
-            $ipAddress = Services::request()->getIPAddress();
-            $this->recordLoginAttempt($credentials['email'] ?? $credentials['username'], $ipAddress, $this->user->id ?? null, false);
-
-            $this->user = null;
             return false;
         }
 
         if ($this->user->isBanned())
         {
             // Always record a login attempt, whether success or not.
-            $ipAddress = Services::request()->getIPAddress();
-            $this->recordLoginAttempt($credentials['email'] ?? $credentials['username'], $ipAddress, $this->user->id ?? null, false);
-
+            $this->recordLoginAttempt($credentials['email'] ?? $credentials['username'], false);
+            
+            // Set error
             $this->error = lang('Auth.userIsBanned');
             
+            // Clear user
             $this->user = null;
+            
             return false;
         }
 
         if (! $this->user->isActivated())
         {
             // Always record a login attempt, whether success or not.
-            $ipAddress = Services::request()->getIPAddress();
-            $this->recordLoginAttempt($credentials['email'] ?? $credentials['username'], $ipAddress, $this->user->id ?? null, false);
+            $this->recordLoginAttempt(false);
 
-            $param = http_build_query([
+            // Set error
+            $this->error = lang('Auth.notActivated') . ' ' . anchor(route_to('resend-activate-account') . '?' . http_build_query([
                 'login' => urlencode($credentials['email'] ?? $credentials['username'])
-            ]);
-
-            $this->error = lang('Auth.notActivated') .' '. anchor(route_to('resend-activate-account').'?'.$param, lang('Auth.activationResend'));
-
+            ]), lang('Auth.activationResend'));
+            
+            // Clear user
             $this->user = null;
+            
             return false;
         }
 
-        return $this->login($this->user, $remember);
+        return $this->login($remember);
     }
+
+    //--------------------------------------------------------------------
 
     /**
      * Checks to see if the user is logged in or not.
@@ -72,15 +75,14 @@ class LocalAuthenticator extends AuthenticationBase implements AuthenticatorInte
             // Do we need to force the user to reset their password?
             if ($this->user && $this->user->force_pass_reset)
             {
-                throw new RedirectException(route_to('reset-password') .'?token='.$this->user->reset_hash);
+                throw new RedirectException(route_to('reset-password') . '?token=' . $this->user->reset_hash);
             }
 
             return true;
         }
 
         // Check the remember me functionality.
-        helper('cookie');
-        $remember = get_cookie('remember');
+        $remember = $this->request->getCookie('remember');
 
         if (empty($remember))
         {
@@ -88,7 +90,6 @@ class LocalAuthenticator extends AuthenticationBase implements AuthenticatorInte
         }
 
         [$selector, $validator] = explode(':', $remember);
-        $validator = hash('sha256', $validator);
 
         $token = $this->loginModel->getRememberToken($selector);
 
@@ -97,63 +98,63 @@ class LocalAuthenticator extends AuthenticationBase implements AuthenticatorInte
             return false;
         }
 
-        if (! hash_equals($token->hashedValidator, $validator))
+        if (! hash_equals($token->hashedValidator, hash('sha256', $validator)))
         {
             return false;
         }
 
-        // Yay! We were remembered!
-        $user = $this->userModel->find($token->user_id);
+        // Get remembered user.
+        $this->user = $this->userModel->find($token->user_id);
 
-        if (empty($user))
+        if (empty($this->user))
         {
             return false;
         }
+        
+        // User were remembered, time for login.
+        $this->login();
 
-        $this->login($user);
-
-        // We only want our remember me tokens to be valid
-        // for a single use.
-        $this->refreshRemember($user->id, $selector);
+        // Set remember me tokens to be valid for a single use.
+        $this->refreshRemember($this->user->id, $selector);
 
         return true;
     }
+
+    //--------------------------------------------------------------------
 
     /**
      * Checks the user's credentials to see if they could authenticate.
      * Unlike `attempt()`, will not log the user into the system.
      *
-     * @param array $credentials
-     * @param bool  $returnUser
+     * @param array $credential
      *
-     * @return bool|User
+     * @return boolean
      */
-    public function validate(array $credentials, bool $returnUser=false)
+    public function validate(array $credential): bool
     {
-        // Can't validate without a password.
-        if (empty($credentials['password']) || count($credentials) < 2)
+        // Can't validate without password.
+        if (empty($credential['password']) || count($credential) !== 2)
         {
             return false;
         }
 
         // Only allowed 1 additional credential other than password
-        $password = $credentials['password'];
-        unset($credentials['password']);
+        $password = $credential['password'];
+        unset($credential['password']);
 
-        if (count($credentials) > 1)
+        if (count($credential) > 1)
         {
             throw AuthException::forTooManyCredentials();
         }
 
         // Ensure that the fields are allowed validation fields
-        if (! in_array(key($credentials), $this->config->validFields))
+        if (! in_array(key($credential), $this->config->validFields))
         {
-            throw AuthException::forInvalidFields(key($credentials));
+            throw AuthException::forInvalidFields(key($credential));
         }
 
         // Can we find a user with those credentials?
-        $user = $this->userModel->where($credentials)
-                                ->first();
+        $user = $this->userModel->where($credential)->first();
 
         if (! $user)
         {
@@ -182,9 +183,8 @@ class LocalAuthenticator extends AuthenticationBase implements AuthenticatorInte
             $this->userModel->save($user);
         }
 
-        return $returnUser
-            ? $user
-            : true;
-    }
+        $this->user = $user;
 
+        return true;
+    }
 }
