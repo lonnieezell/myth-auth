c72dbd1f9ec895d6c65c9739a9d5ce28c6c46aa5
MGatner <mgatner@icloud.com>
2021-06-08 14:11:45 +0000

    Fix test case

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -153,7 +153,7 @@ class AuthController extends Controller
 		// Validate here first, since some things,
 		// like the password, can only be validated properly here.
 		$rules = [
-			'username'  	=> 'required|alpha_numeric_space|min_length[3]|is_unique[users.username]',
+			'username'  	=> 'required|alpha_numeric_space|min_length[3]|max_length[30]|is_unique[users.username]',
 			'email'			=> 'required|valid_email|is_unique[users.email]',
 			'password'	 	=> 'required|strong_password',
 			'pass_confirm' 	=> 'required|matches[password]',
