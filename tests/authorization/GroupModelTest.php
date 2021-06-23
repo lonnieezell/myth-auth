b89bab8ed0f307345080d513e6013d25e4ca5af5
Alex Schmitz <alexander.schmitz@uni-due.de>
2021-06-23 09:49:05 +0200

    Add tests for cache null value return checks.

--- tests/authorization/GroupModelTest.php
+++ tests/authorization/GroupModelTest.php
@@ -115,10 +115,54 @@ class GroupModelTest extends AuthTestCase
         ]);
         $this->assertEquals($result[1], [
             'group_id' => $group2->id,
-            'user_id' => $user->id,
+            'user_id'  => $user->id,
             'name' => $group2->name,
             'description' => $group2->description
         ]);
+    }
+
+    public function testGetGroupsForUserFromCache()
+    {
+        $user   = $this->createUser();
+        $group1 = $this->createGroup();
+        $group2 = $this->createGroup();
+
+        $this->hasInDatabase('auth_groups_users', [
+            'user_id'  => $user->id,
+            'group_id' => $group1->id
+        ]);
+
+        $cacheGroups = [
+            [
+                'group_id' => $group2->id,
+                'user_id' => $user->id,
+                'name' => 'notemptygroup',
+                'description' => 'This group can only be loaded from cache.'
+            ]
+        ];
+        cache()->save("{$user->id}_groups", $cacheGroups, 300);
+
+        $result = $this->model->getGroupsForUser($user->id);
+
+        $this->assertSame($result, $cacheGroups);
+    }
+
+    public function testGetEmptyGroupsForUserFromCache()
+    {
+        $user   = $this->createUser();
+        $group1 = $this->createGroup();
+
+        $this->hasInDatabase('auth_groups_users', [
+            'user_id'  => $user->id,
+            'group_id' => $group1->id
+        ]);
+
+        $cacheGroups = [];
+        cache()->save("{$user->id}_groups", $cacheGroups, 300);
+
+        $result = $this->model->getGroupsForUser($user->id);
+
+        $this->assertSame($result, $cacheGroups);
     }
 
     public function testGetUsersForGroup()
@@ -140,6 +184,47 @@ class GroupModelTest extends AuthTestCase
 
         $this->assertEquals($user1->id, $result[0]['id']);
         $this->assertEquals($user2->id, $result[1]['id']);
+    }
+
+    public function testGetUsersForGroupFromCache()
+    {
+        $group = $this->createGroup();
+        $user1 = fake(UserFaker::class);
+        $user2 = fake(UserFaker::class);
+
+        $this->hasInDatabase('auth_groups_users', [
+            'user_id'  => $user1->id,
+            'group_id' => $group->id
+        ]);
+
+        $cacheUsers = [
+            'group_id' => $group->id,
+            'user_id'  => $user2->id,
+            'email'    => 'gonnaSkip@theOtherProperties.lazy'
+        ];
+        cache()->save("{$group->id}_users", $cacheUsers, 300);
+
+        $result = $this->model->getUsersForGroup($group->id);
+
+        $this->assertSame($result, $cacheUsers);
+    }
+
+    public function testGetEmptyUsersForGroupFromCache()
+    {
+        $group = $this->createGroup();
+        $user = fake(UserFaker::class);
+
+        $this->hasInDatabase('auth_groups_users', [
+            'user_id'  => $user->id,
+            'group_id' => $group->id
+        ]);
+
+        $cacheUsers = [];
+        cache()->save("{$group->id}_users", $cacheUsers, 300);
+
+        $result = $this->model->getUsersForGroup($group->id);
+
+        $this->assertSame($result, $cacheUsers);
     }
 
     public function testAddPermissionToGroup()
